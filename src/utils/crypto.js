83b73b9323085b7a758c71c245ba8aa92a7b5c71
Satish Bhawsar <satishbhawsar92@gmail.com>
2026-03-24 18:38:43 +0530

    crypto updated

--- src/utils/crypto.js
+++ src/utils/crypto.js
@@ -1,24 +1,31 @@
 import crypto from "crypto";
 
+const SECRET = process.env.CRYPTO_SECRET;
+
+if (!SECRET) {
+    throw new Error("CRYPTO_SECRET is missing in .env");
+}
+
+
 const ALGORITHM = "aes-256-cbc";
 const SECRET_KEY = crypto
-  .createHash("sha256")
-  .update(process.env.CRYPTO_SECRET)
-  .digest("base64")
-  .substring(0, 32);
+    .createHash("sha256")
+    .update(SECRET)
+    .digest("base64")
+    .substring(0, 32);
 
 const IV = Buffer.alloc(16, 0); // simple IV (can randomize later)
 
 export const encrypt = (text) => {
-  const cipher = crypto.createCipheriv(ALGORITHM, SECRET_KEY, IV);
-  let encrypted = cipher.update(text, "utf8", "hex");
-  encrypted += cipher.final("hex");
-  return encrypted;
+    const cipher = crypto.createCipheriv(ALGORITHM, SECRET_KEY, IV);
+    let encrypted = cipher.update(text, "utf8", "hex");
+    encrypted += cipher.final("hex");
+    return encrypted;
 };
 
 export const decrypt = (encryptedText) => {
-  const decipher = crypto.createDecipheriv(ALGORITHM, SECRET_KEY, IV);
-  let decrypted = decipher.update(encryptedText, "hex", "utf8");
-  decrypted += decipher.final("utf8");
-  return decrypted;
+    const decipher = crypto.createDecipheriv(ALGORITHM, SECRET_KEY, IV);
+    let decrypted = decipher.update(encryptedText, "hex", "utf8");
+    decrypted += decipher.final("utf8");
+    return decrypted;
 };
